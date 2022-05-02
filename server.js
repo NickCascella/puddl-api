40b1488804e5a5514db98096d6c4ed3ed793ff2e
NickCascella <83091530+NickCascella@users.noreply.github.com>
2022-05-02 17:46:09 -0400

    Simplified queries for joining rooms

--- server.js
+++ server.js
@@ -22,12 +22,13 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   socket.on("online", (data) => {
     con.query(
-      `SELECT users.user_id, chatrooms.chatname FROM users INNER JOIN users_chatrooms ON users.user_id = users_chatrooms.user_id WHERE username = '${data.username}'`,
+      `SELECT chatrooms.chatroom_name FROM users INNER JOIN users_chatrooms ON users.user_id = users_chatrooms.user_id INNER JOIN chatrooms ON users_chatrooms.chatroom_id = chatrooms.chatroom_id WHERE users.username = '${data.username}'`,
       (err, existingUsers) => {
         if (err) {
           console.log(err);
           return;
         }
+
         if (existingUsers.length) {
           existingUsers.forEach(async (chat) => {
             await socket.join(chat.chat_name);
@@ -47,19 +48,19 @@ io.on("connection", (socket) => {
   });
 
   socket.on("join-room", (data) => {
-    const joinChatQuery = (chatroomID, userID) => {
+    const joinChatQuery = (chatroomID) => {
       con.query(
-        `INSERT INTO users_chatrooms (chatroom_id, user_id) VALUES ('${chatroomID}', '${userID}')`,
+        `INSERT INTO users_chatrooms (chatroom_id, user_id) SELECT '${chatroomID}', user_id FROM users WHERE username = '${data.username}'`,
         async (err, _inserted) => {
           if (err) {
             console.log(err);
           }
           const joiningRoom = await socket.join(data.chatroom, "joining rooms");
-
           let getRoomUsers = await io.in(data.chatroom).fetchSockets();
           let usernames = getRoomUsers.map((connection) => {
             return { username: connection.username, online: true };
           });
+
           io.to(data.chatroom).emit("joined-room", {
             joinedRoom: data.chatroom,
             chatUsers: usernames,
@@ -75,54 +76,46 @@ io.on("connection", (socket) => {
       );
     };
 
+    //Check if room exists
     con.query(
-      `SELECT users.user_id FROM users WHERE username = '${data.username}'`,
-      (err, userID) => {
+      `SELECT chatrooms.chatroom_id , chatrooms.chatroom_name FROM chatrooms INNER JOIN users_chatrooms ON users_chatrooms.chatroom_id = chatrooms.chatroom_id INNER JOIN users ON users.user_id = users_chatrooms.user_id WHERE chatroom_name = '${data.chatroom}'`,
+      (err, chatroomData) => {
         if (err) {
           console.log(err);
+          return err;
         }
-        if (!userID.length) {
-          console.log("Invalid user");
-          return;
+
+        if (chatroomData.length) {
+          //Join existing room if user not in it
+          con.query(
+            `SELECT users.user_id, users_chatrooms.chatroom_id FROM users INNER JOIN users_chatrooms ON users.user_id = users_chatrooms.user_id WHERE username = '${data.username}' AND chatroom_id = '${chatroomData[0].chatroom_id}'`,
+            (err, results) => {
+              if (err) {
+                console.log(err);
+                return;
+              }
+              //User in room already, do not join room
+              if (results.length) {
+                console.log("Already in chat");
+                return;
+              }
+              //User not in room, join room
+              joinChatQuery(chatroomData[0].chatroom_id);
+            }
+          );
+        } else {
+          //Create Room and join it
+          con.query(
+            `INSERT INTO chatrooms (chatroom_name) VALUES ('${data.chatroom}')`,
+            (err, chatroomID) => {
+              if (err) {
+                console.log(err);
+                return;
+              }
+              joinChatQuery(chatroomID.insertId);
+            }
+          );
         }
-        con.query(
-          `SELECT chatrooms.chatroom_id FROM chatrooms WHERE chatroom_name = '${data.chatroom}'`,
-          (err, chatroomID) => {
-            if (err) {
-              console.log(err);
-              return;
-            }
-
-            if (chatroomID.length) {
-              con.query(
-                `SELECT users.user_id, users_chatrooms.chatroom_id FROM users INNER JOIN users_chatrooms ON users.user_id = users_chatrooms.user_id WHERE username = '${data.username}' AND chatroom_id = '${chatroomID[0].chatroom_id}'`,
-                (err, results) => {
-                  if (err) {
-                    console.log(err);
-                    return;
-                  }
-
-                  if (results.length) {
-                    console.log("Already in chat");
-                    return;
-                  }
-                  joinChatQuery(chatroomID[0].chatroom_id, userID[0].user_id);
-                }
-              );
-            } else {
-              con.query(
-                `INSERT INTO chatrooms (chatroom_name) VALUES ('${data.chatroom}')`,
-                (err, chatroomID) => {
-                  if (err) {
-                    console.log(err);
-                    return;
-                  }
-                  joinChatQuery(chatroomID.insertId, userID[0].user_id);
-                }
-              );
-            }
-          }
-        );
       }
     );
   });
